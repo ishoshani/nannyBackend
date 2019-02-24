46fa9d26d01a3d8e6f88b4a6e1b75b5633ff7879
Ido Shoshani <ishoshani@Idos-MacBook-Pro.local>
2019-02-23 17:49:35 -0800

    parent flow first draft

--- nanny/urls.py
+++ nanny/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('parentSignUp', views.parentSignUp, name='parentSignUp'),
     path('nannySignUp', views.nannySignUp, name = 'nannySignUp'),
-    path('parentDone', views.parentDone, name = 'parentDone')
+    path('parentProcess', views.parentDone, name = 'parentProcess'),
+    path('parentDone',views.parentDone, name = 'parentProcess')
 ]
--- nanny/views.py
+++ nanny/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+
 
 from .models import Host
 
@@ -14,21 +16,21 @@ def parentSignUp(request):
     template = loader.get_template('nanny/parentSignUp.html')
     context = {}
     return HttpResponse(template.render(context, request))
-def parentDone(request):
+def parentProcess(request):
     info = {
     "username" : request.POST["user_name"],
-    "first name" :  request.POST["first"],
-    "last name" : request.POST["last"],
+    "first_name" :  request.POST["first"],
+    "last_name" : request.POST["last"],
     "phone" : request.POST["phone"],
     "password" : request.POST["password"],
     "location" : ""+request.POST["street"]+" "+request.POST["street2"]+", "+request.POST["city"]+", "+request.POST["state"]
     }
 
-    Host.objects.create(**info);
-    return HttpResponseRedirect(index)
+    id = Host.objects.create(**info);
+    return HttpResponseRedirect("nanny/parentDone")
 
-
-
+def parentDone(request):
+    return render(request, 'nanny/ParentDone.html', {})
 
 def nannySignUp(request):
     template = loader.get_template('nanny/nannySignUp.html')
